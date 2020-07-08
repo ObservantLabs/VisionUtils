c1f7c646d819872aaa133eb7af9315995746c2bd
Alexis Gallagher <alexis@alexisgallagher.com>
2020-07-07 20:05:11 -0700

    wip

--- Tests/VisionUtilsTests/VisionUtilsTests.swift
+++ Tests/VisionUtilsTests/VisionUtilsTests.swift
@@ -1,15 +1,34 @@
+import Foundation
 import XCTest
 @testable import VisionUtils
 
 final class VisionUtilsTests: XCTestCase {
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(DummyStruct().text, "Hello, World!")
-    }
+  func testExample() {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct
+    // results.
+    XCTAssertEqual(DummyStruct().text, "Hello, world")
+  }
 
-    static var allTests = [
-        ("testExample", testExample),
-    ]
+  func testImagePresent() {
+    let imageURL = Bundle.module.url(forResource: "soccerFans", withExtension: "jpg")
+    XCTAssert(imageURL != nil)
+  }
+
+  func testFaceDetect() {
+    guard let imageURL = Bundle.module.url(forResource: "soccerFans", withExtension: "jpg") else {
+      XCTFail("Could not find required test image to run unit tests")
+      return
+    }
+    do {
+      let result = try faceDetect(image: imageURL)
+      print(result)
+    }
+    catch {
+      XCTFail("error trying to run face detection")
+    }
+  }
+  static var allTests = [
+    ("testExample", testExample),
+  ]
 }
