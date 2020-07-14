2f5a34142a62c760747eebd918bc9f2f3c54edba
Alexis Gallagher <alexis@alexisgallagher.com>
2020-07-14 13:08:20 -0700

    refactor. extract closure

--- Sources/VisionUtils/CreateMLAnnotatedImage.swift
+++ Sources/VisionUtils/CreateMLAnnotatedImage.swift
@@ -21,6 +21,7 @@ struct Annotation : Encodable {
   var coordinates:Coordinates
 }
 
+/// coordinates for a rect
 struct Coordinates : Encodable {
   /// centerX with a top-left origin (in pixels)
   var x:Double
@@ -51,44 +52,48 @@ enum AnnotationError : Error {
 func CreateAnnotatedImage(image:URL,observations:[VNFaceObservation]) throws -> AnnotatedImage
 {
   /// (surely there's a more elegant cross-platform way to grab only image dimensions in pixels ?)
-  guard let (imageWidth,imageHeight) = getImageDimensions(image: image) else {
+  guard let imageSize = getImageDimensions(image: image) else {
     throw AnnotationError.errorGetDimensions
   }
 
-  let anns:[Annotation] = observations.map {
-    (fo:VNFaceObservation) -> Annotation in
-
-
-    // TODO: checkout `VNImagePointForNormalizedPoint`
-    // TODO: checkout VNImageRectForNormalizedRect
-
-    // get face bounding box (bottom-left origin, cooordinates normalized to 0...1)
-    let bb = fo.boundingBox
-
-    let boxWidth = Double(bb.width) * Double(imageWidth)
-    let boxHeight = Double(bb.height) * Double(imageHeight)
-
-    let boxLeftBLOrigin = Double(imageWidth) * Double(bb.minX)
-    let boxRightBLOrigin = Double(imageWidth) * Double(bb.maxX)
-    let boxCenterXBLOrigin = (boxLeftBLOrigin + boxRightBLOrigin) / 2.0
-
-    let boxBottomBLOrigin = Double(imageHeight) * Double(bb.minY)
-    let boxTopBLOrigin = Double(imageHeight) * Double(bb.maxY)
-    let boxCenterYBLOrigin = (boxBottomBLOrigin + boxTopBLOrigin) / 2.0
-
-    let boxCenterXLTLOrigin = boxCenterXBLOrigin
-    let boxCenterYTLOrigin = Double(imageHeight) - boxCenterYBLOrigin
-
-    let c = Coordinates(x: boxCenterXLTLOrigin, y: boxCenterYTLOrigin,
-                        width: boxWidth, height: boxHeight)
-
-    let ann = Annotation(label: "face", coordinates: c)
-    return ann
-  }
+  let anns:[Annotation] = observations.map({ createAnnotation(fromObservation: $0, imageSize: imageSize)})
 
   let imageName = image.lastPathComponent
 
   return AnnotatedImage(image: imageName, annotations: anns)
+}
+
+fileprivate func createAnnotation(fromObservation fo:VNFaceObservation,
+                                  imageSize:(UInt, UInt)) -> Annotation
+{
+  let (imageWidth,imageHeight) = imageSize
+
+  // TODO: checkout `VNImagePointForNormalizedPoint`
+  // TODO: checkout VNImageRectForNormalizedRect
+
+
+  // get face bounding box (bottom-left origin, cooordinates normalized to 0...1)
+  let bb = fo.boundingBox
+
+  let boxWidth = Double(bb.width) * Double(imageWidth)
+  let boxHeight = Double(bb.height) * Double(imageHeight)
+
+  let boxLeftBLOrigin = Double(imageWidth) * Double(bb.minX)
+  let boxRightBLOrigin = Double(imageWidth) * Double(bb.maxX)
+  let boxCenterXBLOrigin = (boxLeftBLOrigin + boxRightBLOrigin) / 2.0
+
+  let boxBottomBLOrigin = Double(imageHeight) * Double(bb.minY)
+  let boxTopBLOrigin = Double(imageHeight) * Double(bb.maxY)
+  let boxCenterYBLOrigin = (boxBottomBLOrigin + boxTopBLOrigin) / 2.0
+
+  let boxCenterXLTLOrigin = boxCenterXBLOrigin
+  let boxCenterYTLOrigin = Double(imageHeight) - boxCenterYBLOrigin
+
+  let c = Coordinates(x: boxCenterXLTLOrigin, y: boxCenterYTLOrigin,
+                      width: boxWidth, height: boxHeight)
+
+  let ann = Annotation(label: "face", coordinates: c)
+  return ann
 }
 
 fileprivate func getImageDimensions(image:URL) -> (UInt,UInt)? {
